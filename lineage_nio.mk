e32756ff3a75e0365697dc09a964f1bf25d96baa
vullk4n <vulkanops@gmail.com>
2026-04-30 12:46:22 -0300

    nio: Stay Derp(16.2)
    Change-Id: I5379ed135f075c34579ae81d79f946fea5dc0022

--- lineage_nio.mk
+++ lineage_nio.mk
@@ -12,6 +12,9 @@ PRODUCT_NAME := lineage_nio
 PRODUCT_DEVICE := nio
 PRODUCT_MODEL := XT2125-4
 
+DERPFEST_BUILD_TYPE := Official
+DERPFEST_BUILD_VARIANT := Stable
+
 # Build info
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="nio_retcn-user 12 S1RN32.55-16-13 fce21a-b9f4e9 release-keys" \
